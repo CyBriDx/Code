814c77283397748c7a1f2d81cc02047d4f4671fa
Dushyant Singh <sdushyantp84@gmail.com>
2022-05-22 20:32:08 +0530

    Time: 113 ms (80.40%), Space: 47.2 MB (68.60%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -4,19 +4,14 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    nums1 = nums1.concat(nums2);
-    nums1 = nums1.sort( (a,b) => a-b)
-    let median = 0;
-    let mid = nums1.length%2;
+    nums1 = nums1.concat(nums2).sort( (a,b) => a-b);
     let midInd = Math.trunc(nums1.length/2)
     if(nums1.length === 1) {
-        median = nums1[0];
+        return nums1[0];
     }
     else if (nums1.length%2===0) {
-        median = (nums1[midInd - 1] + nums1[midInd])/2;
+        return (nums1[midInd - 1] + nums1[midInd])/2;
     } else {
-        median = nums1[midInd];
+        return nums1[midInd];
     }
-    console.log(midInd);
-    return median;
 };
