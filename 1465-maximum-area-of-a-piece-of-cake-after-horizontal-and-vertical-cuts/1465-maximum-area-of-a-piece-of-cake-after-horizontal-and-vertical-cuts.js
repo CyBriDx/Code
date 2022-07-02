44c93659896f79ac7d15b669f2fb5854240b6993
Dushyant Singh <sdushyantp84@gmail.com>
2022-07-02 22:06:20 +0530

    Time: 155 ms (79.10%), Space: 53.6 MB (16.42%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} h
+ * @param {number} w
+ * @param {number[]} horizontalCuts
+ * @param {number[]} verticalCuts
+ * @return {number}
+ */
+var maxArea = function(h, w, horizontalCuts, verticalCuts) {
+  const modulo = BigInt(10**9 + 7)
+
+  horizontalCuts = horizontalCuts.sort((a, b) => a-b)
+  verticalCuts = verticalCuts.sort((a, b) => a-b)
+
+  let horizons = [0, ...horizontalCuts, h]
+  let verticals = [0, ...verticalCuts, w]
+
+  let maxH = 0
+  for (let i = 1; i < horizons.length; i++) {
+    maxH = Math.max(maxH, horizons[i] - horizons[i - 1])
+  }
+
+  let maxV = 0
+  for (let i = 1; i < verticals.length; i++) {
+    maxV = Math.max(maxV, verticals[i] - verticals[i - 1])
+  }
+
+  return Number((BigInt(maxV) * BigInt(maxH)) % modulo)
+}
